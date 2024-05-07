e77edf62056f1db43e7a267cb8a8d8836d4a4d04
SushilBose98 <168561371+SushilBose98@users.noreply.github.com>
2024-05-07 13:41:54 +0000

    New File added

---
+++ 01_Basics/01_typeConversion.js
@@ -0,0 +1,14 @@
+let isLoggedIn = "";
+
+let changeTheStatus = String(isLoggedIn);
+console.log(changeTheStatus);
+let a = 5;
+let b;
+
+b = a;
+a++;
+console.log(b);
+let myNumber = "Sushil"
+
+console.log(`My name is ${myNumber.toLocaleLowerCase()}`);
+
